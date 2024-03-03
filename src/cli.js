5155b80f6728efd7c7df5b8fee21a621164a0d64
Igor Matnyak <igormatnak092@gmail.com>
2024-03-03 19:06:39 +0300

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
-export function sayHi () {
-    var userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-}
-
+export default () => {
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
+};
